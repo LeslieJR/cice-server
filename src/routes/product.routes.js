468290fd6397f4d3104c6b182989c6b44bdc6626
LeslieJR <l.reyes.ubiqum@gmail.com>
2022-01-22 22:37:20 +0100

    small change route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,7 +6,7 @@ const controllers = require("../controllers");
 router.post("/create", controllers.product.create);
 router.get("/all", controllers.product.getAll);
 router.get("/byCategory/:category_id", controllers.product.productsByCategory);
-router.get("/byId/:product_id", controllers.product.productById);
+router.get("/details/:product_id", controllers.product.productById);
 router.delete("/remove/:product_id", controllers.product.deleteProduct);
 
 module.exports = router;
