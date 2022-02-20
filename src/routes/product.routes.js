31c766dd4bc91743a0380c61a40f75bda05057c6
LeslieJR <l.reyes.ubiqum@gmail.com>
2022-02-20 13:37:54 +0100

    check for the token to create ot delete a product

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 
 const router = Router();
 const controllers = require("../controllers");
+const helpers = require("../helpers");
 
-router.post("/create", controllers.product.create);
+router.post("/create", helpers.isTokenValid.isTokenValid, controllers.product.create);
 router.get("/recents", controllers.product.getRecents);
 router.get("/category/:category_id", controllers.product.productsByCategory);
 router.get("/details/:product_id", controllers.product.productById);
-router.delete("/remove/:product_id", controllers.product.deleteProduct);
+router.delete("/remove/:product_id", helpers.isTokenValid.isTokenValid, controllers.product.deleteProduct);
 
 module.exports = router;
